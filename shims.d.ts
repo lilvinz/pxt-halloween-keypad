e872a6993081f6ee7fc6e4569ea480d6cd288506
vke <vincent.kessler@quantec-group.com>
2025-10-02 21:58:49 +0200

    enable build

--- shims.d.ts
+++ shims.d.ts
@@ -1,4 +1,11 @@
 // Auto-generated. Do not edit.
+
+
+    /**
+     * Halloween Keypad C++ implementation
+     * Low-level I2C operations for TCA8418
+     */
+
 declare namespace HalloweenKeypad {
 }
 
